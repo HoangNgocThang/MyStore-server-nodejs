2c4f5f34e0e94b9e34f2bbe576ce17b501d8e2f0
thanghoangngoc <thanghn@ominext.com>
2020-12-18 15:50:25 +0700

    sua sql api login

--- src/app/models/Login.js
+++ src/app/models/Login.js
@@ -7,24 +7,36 @@ class Login {
     login(params, callback) {
         console.log(params);
         if (params == null || params == undefined) {
-            callback({err: 'Vui lòng nhập đầy đủ thông tin'});
+            callback({
+                status: 400,
+                err: 'Vui lòng nhập đầy đủ thông tin'
+            });
             return;
         }
 
         if (params.username == '' || params.username == null) {
-            callback({err: 'username không được bỏ trống'});
+            callback({
+                status: 400,
+                err: 'username không được bỏ trống'
+            });
             return;
         }
 
         if (params.password == '' || params.password == null) {
-            callback({err: 'password không được bỏ trống'});
+            callback({
+                status: 400,
+                err: 'password không được bỏ trống'
+            });
             return;
         }
 
-        Database.connection.query(`select * from user where  username = ?`,[params.username], (er, r) => {
+        Database.connection.query(`select * from user where  username = ?`, [params.username], (er, r) => {
             const resultArray = JSON.parse(JSON.stringify(r));
             if (resultArray.length === 0) {
-                callback('username hoặc password không chính xác');
+                callback({
+                    status: 400,
+                    err: 'username hoặc password không chính xác'
+                });
                 return;
             }
             console.log("pass lay trong db:", resultArray[0].password);
@@ -33,6 +45,7 @@ class Login {
                 console.log('pass dung');
                 const token = jwt.sign({username: params.username}, 'mk');
                 callback({
+                    status: 200,
                     access_token: token,
                     username: params.username
                 })
