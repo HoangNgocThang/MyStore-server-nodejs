d2e3543ca4a47a9c9fc63df919383994a7e8da9a
thanghoangngoc <thanghn@ominext.com>
2020-12-18 14:04:50 +0700

    api login

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,22 +1,12 @@
-// const Login = require('../models/Login');
-const jwt = require('jsonwebtoken');
+const Login = require('../models/Login');
 
 class LoginController {
 
     login(req, res) {
-        console.log(req.body.username, req.body.password);
-        try {
-            if (req.body.username === "thang" && req.body.password === '123456') {
-                const token = jwt.sign({username: req.body.username, password: req.body.password}, 'mk');
-                return res.json({
-                    access_token: token,
-                    username: req.body.username
-                })
-            }
-            res.json({err: 'tên tài khoản hoặc mật khẩu k đúng'});
-        } catch (e) {
-            res.json({err: e})
-        }
+        // console.log(req.body.username, req.body.password);
+        Login.login(req.body, (r) => {
+            res.json(r);
+        });
     }
 }
 
--- src/app/models/Login.js
+++ src/app/models/Login.js
@@ -1,8 +1,47 @@
+const jwt = require('jsonwebtoken');
+const Database = require('../../app/db/dbmysql');
+const bcrypt = require('bcrypt');
+
 class Login {
 
-    login() {
+    login(params, callback) {
+        console.log(params);
+        if (params == null || params == undefined) {
+            callback({err: 'Vui lòng nhập đầy đủ thông tin'});
+            return;
+        }
 
+        if (params.username == '' || params.username == null) {
+            callback({err: 'username không được bỏ trống'});
+            return;
+        }
+
+        if (params.password == '' || params.password == null) {
+            callback({err: 'password không được bỏ trống'});
+            return;
+        }
+
+        Database.connection.query(`select * from user where  username = ?`,[params.username], (er, r) => {
+            const resultArray = JSON.parse(JSON.stringify(r));
+            if (resultArray.length === 0) {
+                callback('username hoặc password không chính xác');
+                return;
+            }
+            console.log("pass lay trong db:", resultArray[0].password);
+
+            if (bcrypt.compareSync(params.password, resultArray[0].password)) {
+                console.log('pass dung');
+                const token = jwt.sign({username: params.username}, 'mk');
+                callback({
+                    access_token: token,
+                    username: params.username
+                })
+            } else {
+                console.log('pass sai');
+                callback({err: 'tên tài khoản hoặc mật khẩu k đúng'})
+            }
+        })
     }
 }
 
-module.exports= new Login();
+module.exports = new Login();
