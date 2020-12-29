5748352ad8315502436bf0a3faf365f341a29513
thanghoangngoc <thanghn@ominext.com>
2020-12-29 14:42:45 +0700

    cm

--- src/app/hepler/index.js
+++ src/app/hepler/index.js
@@ -57,42 +57,9 @@ const upload2 = multer({
     }
 }).single("avatar");
 
-// // Set The Storage Engine
-// const storage2 = multer.diskStorage({
-//     destination: './public/upload',
-//     filename: function (req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//     }
-// });
-//
-// // Init Upload
-// const uploadImage = multer({
-//     storage: storage2,
-//     limits: {fileSize: 1000000},
-//     fileFilter: function (req, file, cb) {
-//         checkFileType(file, cb);
-//     }
-// }).single('avatar');
-//
-// // Check File Type
-// function checkFileType(file, cb) {
-//     // Allowed ext
-//     const filetypes = /jpeg|jpg|png|gif/;
-//     // Check ext
-//     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//     // Check mime
-//     const mimetype = filetypes.test(file.mimetype);
-//
-//     if (mimetype && extname) {
-//         return cb(null, true);
-//     } else {
-//         cb('Error: Images Only!');
-//     }
-// }
 
 module.exports = {
     verifyToken,
     upload,
-    // uploadImage,
     upload2
 }
